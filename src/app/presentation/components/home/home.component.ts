7ca3d198c57249e5806a82894267005901973ae9
EstebanRuizz <rubianruiz05@my.com>
2024-05-21 21:00:49 -0500

    Feature: Added home component

--- src/app/presentation/components/home/home.component.ts
+++ src/app/presentation/components/home/home.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherForecastLocationService } from '../../../core/application/services/weather-forecast-location.service';
+import { WeatherForecastLocation } from '../../../core/domain/entities/WeatherForecastLocation';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public weatherLocations: WeatherForecastLocation[] = [];
 
+  constructor(
+    private readonly router: Router,
+    private readonly weatherForecastLocation: WeatherForecastLocationService
+  ) {}
+
+  public ngOnInit(): void {
+    this.loadLocations();
+  }
+
+  public viewForecastByAcronym(id: string): void {
+    this.weatherForecastLocation.getById(id).subscribe({
+      next: (location: WeatherForecastLocation | null) => {
+        location
+          ? this.router.navigate(['weather', location.locationAcronym])
+          : this.notFoundWeatherLocation();
+      },
+      error: (err) => this.notFoundWeatherLocation(),
+    });
+  }
+
+  public notFoundWeatherLocation() {
+    throw new Error('Method not implemented.');
+  }
+
+  private loadLocations() {
+    this.weatherForecastLocation.getAll().subscribe({
+      next: (weatherLocations) => (this.weatherLocations = weatherLocations),
+      error: (err) => console.log('error'),
+    });
+  }
 }
