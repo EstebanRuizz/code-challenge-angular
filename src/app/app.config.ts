08924cd77a43453d7ebabea9b8b388cf6af2f1a1
EstebanRuizz <rubianruiz05@my.com>
2024-05-22 04:09:52 -0500

    Feature: added providers: provideCharts, provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +16,7 @@ export const appConfig: ApplicationConfig = {
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
+    provideCharts(withDefaultRegisterables()),
+    provideHttpClient()
   ],
 };
