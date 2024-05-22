26f1eb78d0b4ae19c12407e8139c4410cc6de5c8
EstebanRuizz <rubianruiz05@my.com>
2024-05-21 19:15:22 -0500

    base routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './presentation/components/home/home.component';
+import { WeatherComponent } from './presentation/components/weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'weather/:location',
+    loadComponent: () =>
+      import('./presentation/components/weather/weather.component').then(
+        (c) => c.WeatherComponent
+      ),
+  },
+];
